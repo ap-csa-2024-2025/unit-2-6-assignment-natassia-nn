8de54816d5c21f95e14585f72aee760b414961bd
natassia-nn <natassianguyen@gmail.com>
2024-10-10 10:07:21 -0700

    completed assignment

--- Main.java
+++ Main.java
@@ -4,6 +4,33 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // write solutions to problems here
+    Scanner sc = new Scanner(System.in);
+    // Problem 1
+    double rad;
+    System.out.println("Enter the radius of the circle: ");
+    rad = sc.nextDouble();
+    Circle c;
+    c = new Circle(rad);
+
+    System.out.println("A circle with the radius of " + rad + " has a circumference of " + c.getCircumference() + " and an area of " + c.getArea());
+
+    // Problem 2
+    int numSide;
+    double sideLength;
+    RegularPolygon polygon;
+
+    System.out.println("Enter number of sides: ");
+    numSide = sc.nextInt();
+    
+    System.out.println("Enter the side length: ");
+    sideLength = sc.nextDouble();
+
+    polygon = new RegularPolygon(numSide, sideLength);
+
+    System.out.println("Area with " + polygon.getNumSides() + " sides: " + polygon.getArea());
+
+    polygon.addSides(2);
+    System.out.println("Incrementing the number of sides by two");
+    System.out.println("Area with " + polygon.getNumSides() + " sides: " + polygon.getArea());
   }
 }
